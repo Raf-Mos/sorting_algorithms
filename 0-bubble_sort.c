e1f7d2ac65f01dcfaaecc814f8f0ed5279544157
Rafiki_Mostafa <rafikimostafa@gmail.com>
2024-02-19 18:34:47 +0100

    update task bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,9 +29,9 @@ void bubble_sort(int *array, size_t size)
 	bool swapped;
 
 	if (!array || !size)
-	return;
+		return;
 
-	 for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
